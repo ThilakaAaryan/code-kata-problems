fc8ae625c749cb5b48c96ffca67dc7119d7773ad
Thilakavathi M <146940878+ThilakaAaryan@users.noreply.github.com>
2024-07-29 00:31:41 +0530

    Create guvi.py

---
+++ guvi.py
@@ -0,0 +1,16 @@
+{"Given a string convert string into upper case
+
+Input Description:
+Given a string
+
+Output Description:
+print the string into uppercase
+
+Sample Input :
+guvi geek
+Sample Output :
+GUVI GEEK"}
+
+
+a=input()
+print(a.upper())
